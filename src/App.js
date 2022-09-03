9c46f88489c3225e353306e53b9a22fe189bb950
harukzz <108406720+harukzz@users.noreply.github.com>
2022-09-03 11:45:26 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo'; 
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '01289cca7254460a9fc68a2d13608e24'
+});
+
+const particlesOptions = {
+                 particles: {
+                  number: {
+                    value: 30,
+                    density: {
+                      enable: true,
+                      value_area: 800
+                }
+              }  
+            }
+          }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ' ',
+      box: {},
+  }
+    }
+
+calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getaElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+}
+
+displayFaceBox = (box) => {
+    console.log (box);
+  this.setState({box: box});
+}
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input});
+  app.models
+  .predict(
+  Clarifai.FACE_DETECT_MODEL,
+    this.state.imput)
+  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  .catch(err => console.log(err));
+}
+
+  render() {
+    return (
+      <div className="App">
+         <Particles className='particles'
+           parans={particlesOptions}            
+          />
+     <Navigation />
+     <Logo />
+     <Rank />
+     <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}
+      />
+     <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+      );
+  }
+}
+export default App;
